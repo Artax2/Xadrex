95573ae313998d78124b859ab3f9be46a708a6d8
Demezio <gustavoblaw1236@gmail.com>
2025-11-28 09:07:15 -0300

    Melhorando a impressão do tabuleiro

--- Program.cs
+++ Program.cs
@@ -9,22 +9,18 @@ namespace xadrex_console {
                 // TAMANHO DO TABULEIRO 
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                // tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                // tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 8));
-                // tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                // Tela.ImprimirTabuleiro(tab);
-                // Console.ReadLine();
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-                PosicaoXadrex pos = new PosicaoXadrex('c', 7);
-
-                Console.WriteLine(pos);
-
-                Console.WriteLine(pos.toPosicao());
+                Tela.ImprimirTabuleiro(tab);
             }
-            catch(TabuleiroException e) {
+            catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
             }
+            Console.ReadLine();
         }
     }
 }
--- Tela.cs
+++ Tela.cs
@@ -5,6 +5,7 @@ namespace xadrex_console {
         public static void ImprimirTabuleiro(Tabuleiro tab) {
             // PADRÃO USADO PARA PERCORRER MATRIZES
             for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++) {
                     // CASO NÃO TENHA PEÇA
                     if (tab.peca(i, j) == null) {
@@ -12,11 +13,25 @@ namespace xadrex_console {
                     }
                     // CASO TENHA PEÇA
                     else {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+         public static void ImprimirPeca(Peca peca) {
+            if (peca.cor == Cor.Branca) {
+                Console.Write(peca);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
